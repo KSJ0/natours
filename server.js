ca36d98dfded180125ed9f143e548861717a0c1a
Kevyn San Juan <ksanjuan.000@gmail.com>
2024-05-17 16:31:09 +0800

    database connection

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+// DATABASE CONNECTION
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // SERVER
 const port = process.env.PORT || 3000;
